0d67747fd99d2c8f3bec5b8f1eb298a0b75b7150
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-10-12 23:33:15 +0300

    Add examples of use

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from decorators import memoize, profile
+import sys
+
+from decorators import debug, once, deprecated, memoize, profile
+
+
+@debug(stream=sys.stderr)
+def minmax(*args):
+    min_result = max_result = float("-inf")
+    for arg in args:
+        min_result = arg if arg < min_result else min_result
+        max_result = arg if arg > max_result else max_result
+    return min_result, max_result
+
+
+@once
+def init_logger():
+    pass
+
+
+@deprecated(replacement=init_logger)
+def activate_logger():
+    pass
 
 
 @profile
 @memoize
 def repeat(n):
-    for i in range(n):
+    for _ in range(n):
         pass
-
-
-repeat(100_000_000)
-print(repeat.__total_time__)
-repeat(100_000_000)
-print(repeat.__total_time__)
