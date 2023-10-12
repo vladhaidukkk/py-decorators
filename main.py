1693f3b434a803a9f096c7c4b1d4afe6f19e01be
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-10-12 19:21:14 +0300

    Allow to pass arguments to debug

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import functools
+import sys
 
 
 DEBUG = True
 
 
-def debug(fn):
-    if not DEBUG:
-        return fn
+def debug(stream=sys.stdout):
+    def decorator(fn):
+        if not DEBUG:
+            return fn
 
-    @functools.wraps(fn)
-    def inner(*args, **kwargs):
-        params = ", ".join(
-            [str(arg) for arg in args] + [f"{key}={value}" for key, value in kwargs.items()]
-        )
-        print(f"{fn.__name__}({params}) = ?")
-        result = fn(*args, **kwargs)
-        print(f"{fn.__name__}({params}) = {result}")
-        return result
+        @functools.wraps(fn)
+        def inner(*args, **kwargs):
+            params = ", ".join(
+                [str(arg) for arg in args] + [f"{key}={value}" for key, value in kwargs.items()]
+            )
+            print(f"{fn.__name__}({params}) = ?", file=stream)
+            result = fn(*args, **kwargs)
+            print(f"{fn.__name__}({params}) = {result}", file=stream)
+            return result
 
-    return inner
+        return inner
+
+    return decorator
 
 
-@debug
+@debug(sys.stderr)
 def minmax(first, *rest):
     min_result = max_result = first
     for arg in rest:
