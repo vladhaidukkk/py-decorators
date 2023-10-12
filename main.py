f0ce267df90dde4b31efd46a08d8e8c29adf001b
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-10-12 18:48:51 +0300

    Create basic debug decorator

---
+++ main.py
@@ -0,0 +1,23 @@
+def debug(fn):
+    def inner(*args, **kwargs):
+        params = ", ".join(
+            [str(arg) for arg in args] + [f"{key}={value}" for key, value in kwargs.items()]
+        )
+        print(f"{fn.__name__}({params}) = ?")
+        result = fn(*args, **kwargs)
+        print(f"{fn.__name__}({params}) = {result}")
+        return result
+
+    return inner
+
+
+@debug
+def minmax(first, *rest):
+    min_result = max_result = first
+    for arg in rest:
+        min_result = arg if arg < min_result else min_result
+        max_result = arg if arg > max_result else max_result
+    return min_result, max_result
+
+
+minmax(1, 3, 9, 5, 7)
